343bfa109277e5922e1c401a69743dda7ed9e195
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-04-06 20:41:08 +0100

    listen on all interfaces

--- src/inferadmin/__init__.py
+++ src/inferadmin/__init__.py
@@ -1,6 +1,10 @@
 def main() -> None:
     import uvicorn
-    uvicorn.run(app="inferadmin.main:app")
+    uvicorn.run(
+        app="inferadmin.main:app",
+        host="0.0.0.0",
+        port=8000,
+    )
 
 if __name__ == "__main__":
     main()
