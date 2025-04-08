03a7c4ae0e415cc67585ce84728a6f48a7b5f923
Ben Gitter <gitterbd@gmail.com>
2025-04-08 00:08:16 +0000

    implemented /infra/volumes GET

--- src/inferadmin/routes/infra/volumes/__init__.py
+++ src/inferadmin/routes/infra/volumes/__init__.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter
 from .models import GetVolumesResponse
+from pathlib import Path
+from datetime import datetime
+import shutil
+
+from .models import Volume
+
+from inferadmin.config.loader import config_manager
 
 router = APIRouter(
     prefix='/volumes'
@@ -7,4 +14,23 @@ router = APIRouter(
 
 @router.get('/')
 async def get_volumes() -> GetVolumesResponse:
-    pass
+    volume_path = Path(config_manager.get_config().model_storage_path)
+    # Grab total space in GB
+    total_space = shutil.disk_usage(volume_path).total / (1024 ** 3)
+    # Grab Used space in GB
+    used_space = shutil.disk_usage(volume_path).used / (1024 ** 3)
+    # Grab Date Created
+    date_created = volume_path.stat().st_ctime
+    # Convert to datetime
+    date_created = datetime.fromtimestamp(date_created)
+
+    volume = Volume(
+        name=volume_path.name,
+        total_volume_size=total_space,
+        volume_size_used=used_space,
+        date_created=date_created
+    )
+    response = GetVolumesResponse(
+        volumes=[volume]
+    )
+    return response
